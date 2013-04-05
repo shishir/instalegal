ab55bcb2057a13da87c77f9ab00ee94b129948d2
Akshay & Ketan <akshay_and_ketan@snap-ci.com>
2013-04-05 22:22:26 +0530

    Shishir/Sahil: added bootstrap and landing page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+
+  def full_title(page_title)
+    base_title = "Instalegal"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
+
+  def navbar_item(text, path)
+    opts = {}
+    opts[:class] = 'active' if current_page?(path)
+    content_tag(:li, opts) do
+      link_to(text, path)
+    end
+  end
 end
