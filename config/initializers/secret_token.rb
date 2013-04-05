95439bef8cd8a7479e6ab34fe579d5b4539afa0a
Akshay & Ketan <akshay_and_ketan@snap-ci.com>
2013-04-05 21:45:50 +0530

    Sahil/Shishir: rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Instalegal::Application.config.secret_token = '42da45d20f6e1dd586f14449b3d835ccf204212bd1955f6e1c7cb9271260506a5fb66e5cccdeadec34dffc4802132c0f41c861f8ada62227cc6bcb37e55d3964'
